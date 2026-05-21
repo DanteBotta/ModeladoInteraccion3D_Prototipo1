15387ff8df0f08318ca53fae8822c3d24ec2ff03
Damptermite4040 <dantegiovannibotta@gmail.com>
2026-05-21 12:02:40 -0300

    Clase 3: Implementación de GameManager y UIManager con TextMeshPro

---
+++ Assets/Scripts/InteractiveArea.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractiveArea : MonoBehaviour
+{
+    public UIManager UIManager;
+    public int Score = 0;
+
+    void Awake()
+    {
+        UIManager = GameObject.FindObjectOfType<UIManager>();
+    }
+
+    private void OnTriggerEnter(Collider other) 
+    { 
+        if (other.gameObject.CompareTag("Pickable")) 
+        { 
+            Destroy(other.gameObject); 
+            Score += 1;
+            UIManager.UpdateScore(Score);
+        } 
+    }
+
+
+    // public GameObject imagenUI;
+    // GameObject objetoActual;
+
+    // void Start()
+    // {
+        // imagenUI.SetActive(false);
+    // }
+
+    // void OnTriggerEnter(Collider other)
+    // {
+        // if (other.CompareTag("Pickable"))
+        // {
+            // objetoActual = other.gameObject;
+            // imagenUI.SetActive(true);
+        // }
+    // }
+
+    // void OnTriggerExit(Collider other)
+    // {
+        // if (other.CompareTag("Pickable"))
+        // {
+            // imagenUI.SetActive(false);
+            // objetoActual = null;
+        // }
+    // }
+
+    // void Update()
+    // {
+        // if (Input.GetKeyDown(KeyCode.E) && objetoActual != null)
+        // {
+            // Destroy(objetoActual);
+            // imagenUI.SetActive(false);
+        // }
+    // }
+}
---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class UIManager : MonoBehaviour
+{
+    public TextMeshProUGUI TextoScore;
+    public TextMeshProUGUI TextoTimer;
+    float Tiempo = 30f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        UpdateScore(0);
+    }
+
+    public void UpdateScore(int score)
+    {
+        TextoScore.text = "Score: " + score.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Tiempo -= Time.deltaTime;
+        TextoTimer.text = "Tiempo: " + Tiempo.ToString("F1");
+
+    }
+}
