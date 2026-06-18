f711dc27707db1e5c25f78a3fc2f8e7b3ebce76e
Damptermite4040 <dantegiovannibotta@gmail.com>
2026-06-18 11:02:05 -0300

    Cambiamos mucho

---
+++ Assets/Scripts/RotationVentilator.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationVentilator : MonoBehaviour
+{
+    public int VelocidadRotacion = 5;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, VelocidadRotacion, 0);
+    }
+}
